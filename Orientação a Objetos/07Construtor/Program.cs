39d13d728c4a499cfa34ac4b96886e56de6135dc
Thiago Pinto <91860056+ThiagoPinto87@users.noreply.github.com>
2023-06-28 16:45:12 -0400

    Update Program.cs
    Quebra de linha da primeira linha do comentário.

--- Orientação a Objetos/07Construtor/Program.cs
+++ Orientação a Objetos/07Construtor/Program.cs
@@ -3,7 +3,8 @@
 
 Pessoa objConstrutor = new Pessoa("Thiago");
 /*
-A característica do construtor é que ele é executado *assim que a classe é instanciada* (ou seja, ao instanciar a classe o construtor já está sendo executado).
+A característica do construtor é que ele é executado *assim que a classe é instanciada* 
+(ou seja, ao instanciar a classe o construtor já está sendo executado).
 
 Para testar, basta colocar como parâmetro da Pessoa, seu nome. (colocar o nome entre os parênteses).
 */
